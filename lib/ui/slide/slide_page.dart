9b2d0c7331c906f76f1a842b57cba41f6600cc98
asmz <land.front.high.field@gmail.com>
2025-03-04 00:33:29 +0900

    chore: Add tmp slide page

--- lib/ui/slide/slide_page.dart
+++ lib/ui/slide/slide_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio_app_flutter/models/post/post_request.dart';
+import 'package:portfolio_app_flutter/provider/tumblr_api_provider.dart';
 
-class SlidePage extends StatelessWidget {
+class SlidePage extends ConsumerWidget {
   const SlidePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('This is SlidePage', style: TextStyle(fontSize: 32.0)),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final response = ref.watch(
+      tumblrApiProvider(PostRequest(offset: 0, limit: 10, tag: 'slide')),
+    );
+
+    return Container(
+      child: response.when(
+        data: (data) {
+          return Column(
+            children: [
+              for (var post in data.posts)
+                Text(post.content[0].title ?? "empty"),
+            ],
+          );
+        },
+        loading: () => Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) => Center(child: Text('Error: $error')),
+      ),
     );
   }
 }
